2b7ab96ce0bc03f42bb753193266a909021382ac
giuseppemaisto <gmaisto98@gmail.com>
2023-01-23 14:31:42 +0100

    inserito array navbar e props

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+ <script>
+    import AppNav from './AppNav.vue';
+    export default {
+        components:{
+            AppNav
+        },
+            data(){
+                return{
+                    Navbar:[
+                        {
+                            label: 'HOME',
+                        },
+                        {
+                            label: 'FIXTURES & RESULS'
+                        },
+                        {
+                            label: 'LEAGUE TABLE'
+                        },
+                        {
+                            label: 'PLAYERS'
+                        },
+                        {
+                            label:'GALLERY',
+
+                        },
+                        {
+                            label:'BLOG'
+
+                        },
+                        {
+                            label:'CONTACT'
+                        },
+                    ]
+                }
+            }
+    }
+ </script>
+<template lang="">
+    <div>
+        <AppNav :Navbar="Navbar"/>
+    </div>
+</template>
+<style lang="">
+    
+</style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,23 @@
+        <script>
+        export default {
+            props:{
+                Navbar: Array,
+            }
+        }
+        </script>
+<template lang="">
+    <div>
+        <nav class="navbar">
+            <ul>
+                <li v-for="(item, index) in Navbar" :key="index">
+                    <a href="#">
+                        {{item.label}}
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+<style lang="">
+    
+</style>
