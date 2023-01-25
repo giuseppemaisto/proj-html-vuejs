3788187f589df13ee11dc82c33e866f2a209a12d
giuseppemaisto <gmaisto98@gmail.com>
2023-01-25 11:38:14 +0100

    inserito array match

---
+++ src/components/AppCalendarMatch.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    props:{
+        arrayMatch: array
+    }
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,68 @@
 <script>
 import AppCarousel from './AppCarousel.vue';
 import AppMatch from './AppMatch.vue';
+import AppCalendarMatch from './AppCalendarMatch.vue'
 export default {
         components:{
             AppCarousel,
             AppMatch,
+            AppCalendarMatch,
         },
+        data(){
+            return{
+                ArrayMatch:[
+                    {
+                        imgSquadra1:'/../public/img/club-3.png',
+                        nameSquadra1:'Schalke Club',
+                        nameSquadra2:'/../public/img/club-4.png',
+                        imgSquadra2:'Inter Club',
+                        data:'October 20, 2022 17:00 pm',
+                        luogo:'Expert Stadium',
+                    },
+                    {
+                        imgSquadra1:'/../public/img/club-9.png',
+                        nameSquadra1:'Atletico Club',
+                        nameSquadra2:'/../public/img/club-6.png',
+                        imgSquadra2:'Schalke Club',
+                        data:'September 18, 2022 21:00pm',
+                        luogo:'Expert Stadium',
+                    },
+                    {
+                        imgSquadra1:'/../public/img/club-2.png',
+                        nameSquadra1:'United Fs Club',
+                        nameSquadra2:'Bayern Club',
+                        imgSquadra2:'/../public/img/club-8.png',
+                        data:'August 3, 2022 19:00 pm',
+                        luogo:'Expert Stadium'
+                    },
+                    {
+                        imgSquadra1:'/../public/img/club-7.png',
+                        nameSquadra1:'Arsenal Club',
+                        nameSquadra2:'Atletico Club',
+                        imgSquadra2:'/../public/img/club-9.png',
+                        data:'July 8, 2022 12:00 pm',
+                        luogo:'Expert Stadium',
+                    },
+                    {
+                        imgSquadra1:'/../public/img/league-club-1.png',
+                        nameSquadra1:'Instabul Sports',
+                        nameSquadra2:'United Fs Club',
+                        imgSquadra2:'/../public/img/club-2.png',
+                        data:'june 12, 2022 09:00 am',
+                        luogo:'Expert Stadium',
+                    },
+                ]
+            }
+        }
        
 }
 </script>
 <template lang="">
 <AppCarousel></AppCarousel>
 <AppMatch></AppMatch>
+<AppCalendarMatch></AppCalendarMatch>
 </template>
 <style lang="scss" scoped>
-*{
-    
-}
+
 
 </style>
